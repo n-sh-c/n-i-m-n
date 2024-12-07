aab092dca0be92cf3b5a536940012a6fb88d4191
naser <naser.code18@gmail.com>
2024-12-07 22:01:44 +0330

    ip

--- network.py
+++ network.py
@@ -3,3 +3,18 @@ from termcolor import colored
 result = colored(pyfiglet.figlet_format('naser', font = 'banner3-D'), 'white')
 print(result)
 print('N-I-M-N'.center(50))
+print('[1] My Ip Address')
+print('[2] My Mac Address')
+print('[3] Range Ip Address (Network Lan)')
+print('[4] Exit')
+input_user = input('Select the desired option : ')
+while True :
+    if input_user == '1' :
+        print('[1] Ip Address (Wireless)')
+        print('[2] Ip Address (Ethernet)')
+        input_ip = input('Select the desired option : ')
+        if input_ip == '1' :
+            pass 
+        elif input_ip == '2' :
+            pass 
+        
